c2967be88db62449fd7b942940df62f673294497
Cipher <108844893+modulelife@users.noreply.github.com>
2025-08-07 17:44:53 +0800

    Alpha 0.1.2 25w32a

---
+++ binary_release/Qcdtr1/include/qcd_enum.h
@@ -0,0 +1,51 @@
+#ifndef QCD_ENUM_H
+#define QCD_ENUM_H
+
+
+#define QCD_H											10000
+#define QCD_NOT											10001
+#define QCD_X											10001
+#define QCD_Y											10002
+#define QCD_Z											10003
+#define QCD_sqrtX										10004
+#define QCD_sqrtXdag									10005
+#define QCD_sqrtY										10006
+#define QCD_sqrtYdag									10007
+#define QCD_S											10008
+#define QCD_T											10009
+#define QCD_Sdag										10010
+#define QCD_Tdag										10011
+#define QCD_P0											10012
+#define QCD_P1											10013
+#define QCD_RX											10014
+#define QCD_RY											10015
+#define QCD_RZ											10016
+#define QCD_InvRX										10017
+#define QCD_InvRY										10018
+#define QCD_InvRZ										10019
+
+#define QCD_BIT_FLIP									10100
+#define QCD_DEPHASING									10101
+#define QCD_DEPOLARIZING								10103
+#define QCD_INDEPENDENT_XZ								10104
+
+
+#define QCD_NO_ERROR									20000
+#define QCD_INVALID_CONTEXT								20001
+#define QCD_INVALID_QUBIT								20002
+#define QCD_SEGMENT_NOT_EXIST							20003
+#define QCD_INVALID_GATE_TYPE							20004
+#define QCD_NOT_INITIALIZED								20005
+#define QCD_NOT_IMPLEMENTED								20006
+#define QCD_INVALID_PARAMETER							20007
+#define QCD_UNDONE_OPERATION							20008
+#define QCD_EXECUTION_FAILURE							20009
+#define QCD_RESULT_UNAVAILABLE							20010
+#define QCD_INVALID_BASE_INDEX							20011
+#define QCD_OUT_OF_MEMORY								20012
+#define QCD_INVALID_NOISE_TYPE							20013
+
+
+
+
+#endif
