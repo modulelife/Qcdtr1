882ccf168254caedde58e0d47546263abc474f20
Cipher <108844893+modulelife@users.noreply.github.com>
2025-08-14 17:28:58 +0800

    Alpha 0.3.1 25w33b

---
+++ QcdDemo/src/tests/shortest.cpp
@@ -0,0 +1,29 @@
+#define _USE_MATH_DEFINES
+#include "shortest.h"
+#include "benchmark.h"
+#include <iostream>
+#include <numeric>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <random>
+#include "qcd_api.h"
+#include "qcd_debug.h"
+
+
+
+
+
+
+void ShorTest::run()
+{
+    std::cout << "\n>..ShorTest: begin" << std::endl;
+    Benchmark_Begin(shor_test);
+
+
+    std::cout << "\nhaven't done yet\n";
+
+
+    std::cout << "\n>..ShorTest: end" << std::endl;
+    Benchmark_End(shor_test);
+}
